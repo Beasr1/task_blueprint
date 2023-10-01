ad878d7e08a6c5e1714ae33a44aa010b5ef2d5d5
Beasr1 <d.ishaan001@gmail.com>
2023-10-02 02:21:41 +0530

    displayName check

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,24 +1,22 @@
 import React from "react";
 import { forwardRef } from "react";
-//import { twMerge } from "tailwind-merge";
 
-// placeholder:text-neutral-400
-//border-transparent
-const Input = React.forwardRef(
-  ({ className, type, disabled, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className="bg-background_color
+const Input = forwardRef(({ type, disabled, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className="bg-background_color
         disabled:cursor-not-allowed 
-            disabled:opacity-50
-            focus:outline-none"
-        disabled={disabled}
-        ref={ref}
-        {...props}
-      />
-    );
-  }
-);
+        disabled:opacity-50
+        focus:outline-none"
+      disabled={disabled}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+
+// Add a display name to your component
+Input.displayName = "Input";
 
 export default Input;
